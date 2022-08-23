c616574a51fae808c49e96dcda3f75c8e1060f49
AliceBGott <88068981+AliceBGott@users.noreply.github.com>
2022-08-23 10:46:33 -0300

    feat: alurapic - criando componentes

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+ <div class="corpo">
+    <h1 class="centralizado">{{titulo}}</h1>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+        <img v-bind:src="foto.url" v-bind:alt="foto.titulo">
+
+        <meu-painel :titulo="foto.titulo">
+           <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo"></img>
+        </meu-painel>
+
+         
+      </li>
+     
+      
+    </ul>
+    
+ </div>
+ 
+</template>
+
+<script>
+import Painel from './components/shared/painel/painel.vue';
+export default {
+  components: {
+        'meu-painel' : Painel
+    },
+  data (){
+
+    return {
+      titulo: 'Alurapic',
+      fotos: []
+      
+    }
+  },
+
+  created(){
+    let promise = this.$http.get('http://localhost:3000/v1/fotos');
+    promise
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+  }
+}
+</script>
+
+<style>
+  .centralizado{
+    text-align: center;
+  }
+
+  .corpo{
+    font-family: Helvetica, sans-serif;
+    width:96%;
+    margin: 0, auto;
+  }
+
+  .lista-fotos{
+    list-style: none;
+  }
+
+  .lista-fotos-item{
+    display: inline-block;
+  }
+
+  .imagem-responsiva{
+    width: 100%
+  }
+
+    
+</style>
