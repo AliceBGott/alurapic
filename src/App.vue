bd2100ebea9a97a81103a4be3ebfbe0852eb7b1d
AliceBGott <88068981+AliceBGott@users.noreply.github.com>
2022-08-24 18:30:31 -0300

    feat: filtro de imagens

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
  <div class="corpo">
     <h1 class="centralizado">{{titulo}}</h1>
 
-    <input type="search" class="filtro" v-on:input="filtro=$event.target.value" placeholder="filtre pelo titulo"></input>
+    <input type="search" class="filtro" v-on:input="filtro=$event.target.value" placeholder="filtre pelo titulo">
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
-        <img v-bind:src="foto.url" v-bind:alt="foto.titulo">
+       
+
 
         <meu-painel :titulo="foto.titulo">
-           <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo"></img>
+           <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
         </meu-painel>
 
          
@@ -40,7 +41,8 @@ export default {
   computed: {
     fotosComFiltro(){
       if(this.filtro){
-        return [];
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
       }else{
         return this.fotos;
       }
