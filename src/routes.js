5c532b9172e44bb19d150a654418484a08bb9972
AliceBGott <88068981+AliceBGott@users.noreply.github.com>
2022-08-25 23:29:01 -0300

    feat:  menu

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,11 @@
 import Home from './components/home/Home.vue';
 import Cadastro from './components/cadastro/Cadastro.vue';
 
-export const routes = [
-    { path: '', component: Home },
-    { path: '/cadastro', component: Cadastro }
+// adicionando a propriedade título
 
-  ];
+export const routes = [
+
+    { path: '', component: Home, titulo: 'Home' },
+    { path: '/cadastro', component: Cadastro, titulo: 'Cadastro' }
+
+];
